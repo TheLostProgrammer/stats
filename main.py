62fadc5fb8ae4fd66391b0f7e46e5b643814deff
TheLostProgrammer <77237673+TheLostProgrammer@users.noreply.github.com>
2021-10-18 14:11:19 +1030

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def calculate(title, numbers):
     q3 = q3[len(q3) // 2] if len(q3) % 2 != 0 else (q3[len(q3) // 2] + q3[len(q3) // 2 - 1]) / 2
     iqr = q3 - q1
 
-    df = pd.DataFrame(data=numbers, columns=title)
+    df = pd.DataFrame(data=numbers, columns=[title])
     sns.boxplot(y="variable", x="value", data=pd.melt(df))
     plt.show()
     return f"Sorted: {', '.join(str(i) for i in numbers)}\nQ1: {q1}\nQ2/Median: {q2}\nQ3: {q3}\nIQR: {iqr}\nLower Outlier(s): {', '.join(str(i) for i in [i for i in numbers if i < q1 - (1.5 * iqr)])}\nUpper Outlier(s): {', '.join(str(i) for i in [i for i in numbers if i > q3 + (1.5 * iqr)])}\nMin: {min(numbers)}\nMax: {max(numbers)}\nRange: {max(numbers) - min(numbers)}\nMean: {round(sum(numbers) / len(numbers), 2)}"
